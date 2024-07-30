1c3463727c566f22f8fa6a0a54bf5fa22b269aed
Eunsoojoe <joeun2264@gmail.com>
2024-07-30 15:03:44 +0900

    Update기능 구현

--- articles/views.py
+++ articles/views.py
@@ -39,4 +39,23 @@ def delete(reqeust, id):
 
     return redirect('articles:index')
 
+def edit(request, id):
+    article = Article.objects.get(id=id)
 
+    context = {
+        'article':article,
+    }
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    # 기존 정보 가져오기
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # 새로운 정보 수정
+    article = Article.objects.get(id=id)
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect('articles:detail', id=article.id)
