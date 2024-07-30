fe5a4ed16e17bf55e47152016ca7a2ccb7cf00ca
Eunsoojoe <joeun2264@gmail.com>
2024-07-30 10:42:46 +0900

    모델링,마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# 모델링 & 스키마 정의
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # *auto_now_add = True : 실제로 작성한 시점을 자동으로 입력
+    # *auto_now : 수정된 시간 정보를 자동으로 반영
+
+
